50f3a6558afcdf1d7773afb9886983615165642f
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-04-08 22:38:08 +0900

    [feat] : 페이지 라우팅 연결

---
+++ src/components/Router.js
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Main from '../pages/Main';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+        </Routes>
+    )
+}
+
+export default Router;
