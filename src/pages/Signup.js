0a337fe63aed622cab9e29749e8073ae0b217a42
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-04-11 17:48:57 +0900

    [feat] : 처음 페이지에 접속했을 때 제외하고 조건에 따라 에러 메세지 출력 구현

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Signup.css';
 
 const Signup = () => {
@@ -8,7 +8,7 @@ const Signup = () => {
 
     const [id, setId] = useState('');
     const [idValid, setIdValid] = useState(false);
-    const [idCheck, setIdCheck] = useState(true);
+    const [idCheck, setIdCheck] = useState(false);
     const [idMessage, setIdMessage] = useState('');
 
     const [pw, setPw] = useState('');
@@ -38,48 +38,46 @@ const Signup = () => {
         //아이디 중복확인 api 로직 
         setIdCheck(true);
         setIdMessage('사용 가능한 아이디입니다.');
-
-        // if (idCheck) {
-        //     setIdMessage('사용 가능한 아이디입니다.');
-        // } else {
-        //     setIdMessage('이미 사용중인 아이디입니다.');
-        // }
     }
 
     useEffect(() => {
-        const nicknameRegex = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]{2,10}$/;
-        if (nicknameRegex.test(nickname)) {
-            setNicknameValid(true);
-            setNicknameMessage('');
-        } else {
-            setNicknameValid(false);
-            setNicknameMessage('닉네임은 2-10자의 한글, 영문, 숫자로 구성되어야 합니다.');
+        if (nickname !== '') {
+            const nicknameRegex = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]{2,10}$/;
+            if (nicknameRegex.test(nickname)) {
+                setNicknameValid(true);
+            } else {
+                setNicknameValid(false);
+                setNicknameMessage('닉네임은 2-10자의 한글, 영문, 숫자로 구성되어야 합니다.');
+            }
         }
     }, [nickname]);
 
     useEffect(() => {
-        const idRegex = /^[a-z0-9]{5,19}$/g;
-        if (idRegex.test(id)) {
-            setIdValid(true);
-        } else {
-            setIdValid(false);
+        if (id !== '') {
+            const idRegex = /^[a-z0-9]{5,19}$/g;
+            if (idRegex.test(id)) {
+                setIdValid(true);
+            } else {
+                setIdValid(false);
+            }
         }
     }, [id]);
 
     useEffect(() => {
-        const passwordRegex = /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#.~_-])[A-Za-z\d@$!%*?&#.~_-]{8,20}$/;
-        if(passwordRegex.test(pw)) {
-            setPasswordValid(true);
-            setPasswordMessage('');
-        } else {
-            setPasswordValid(false);
-            setPasswordMessage('비밀번호는 8-20자의 영문, 숫자, 특수문자로 구성되어야 합니다.');
-        }
+        if (pw !== '' || passwordCheck !== '') {
+            const passwordRegex = /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#.~_-])[A-Za-z\d@$!%*?&#.~_-]{8,20}$/;
+            if (passwordRegex.test(pw)) {
+                setPasswordValid(true);
+            } else {
+                setPasswordValid(false);
+                setPasswordMessage('비밀번호는 8-20자의 영문, 숫자, 특수문자로 구성되어야 합니다.');
+            }
 
-        if (pw === passwordCheck) {
-            setPasswordMatch(true);
-        } else {
-            setPasswordMatch(false);
+            if (pw === passwordCheck) {
+                setPasswordMatch(true);
+            } else {
+                setPasswordMatch(false);
+            }
         }
     }, [pw, passwordCheck]);
 
@@ -94,9 +92,7 @@ const Signup = () => {
     return (
         <div className='signupPage'>
             <div className='signupPageContent'>
-                <div className='inputMessageWrap'>
-                    {!nicknameValid && <div className='inputMessage'>{nicknameMessage}</div>}
-                </div>
+                {!nicknameValid && nickname !== '' && <div className='inputMessage'>{nicknameMessage}</div>}
                 <div className='signupInputWrap'>
                     Nickname
                     <input 
@@ -107,21 +103,19 @@ const Signup = () => {
                         onChange={handleNickname} />
                 </div>
 
-                {idCheck ? 
-                    <div className='inputMessage'>사용 가능한 아이디입니다.</div> 
-                    : <div className='inputMessage'>이미 사용중인 아이디입니다.</div>}
+                {idCheck && <div className='inputMessage'>{idMessage}</div>}
                 <div className='signupInputWrap' style={{marginBottom:'0'}}>
                     id
                     <input 
                         className='signupInput' 
                         type='text'
-                        placeholder='아이디 입력 (영문, 숫자 포함 6-20자) '
+                        placeholder='아이디 입력 (영문, 숫자 포함 5-19자) '
                         value={id}
                         onChange={handleId} />
                 </div>
                 <button className='checkDuplicateButton' onClick={handleIdCheck}>중복확인</button>
 
-                {!passwordValid && <div className='inputMessage'>{passwordMessage}</div>}
+                {!passwordValid && (pw !== '' || passwordCheck !== '') && <div className='inputMessage'>{passwordMessage}</div>}
                 <div className='signupInputWrap'>
                     pw
                     <input 
@@ -132,7 +126,7 @@ const Signup = () => {
                         onChange={handlePw} />
                 </div>
 
-                {!passwordMatch && <div className='inputMessage'>비밀번호가 일치하지 않습니다.</div>}
+                {!passwordMatch && (pw !== '' || passwordCheck !== '') && <div className='inputMessage'>비밀번호가 일치하지 않습니다.</div>}
                 <div className='signupInputWrap'>
                     Confirm pw
                     <input 
